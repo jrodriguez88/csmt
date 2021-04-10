6916b0e4fba0da99dccdf728036b70382999fc7e
jrodriguez88 <jeferson_88@hotmail.com>
2021-04-09 21:23:45 -0500

    adv. soilgrids v2

--- get_data/get_data_soilgrids.R
+++ get_data/get_data_soilgrids.R
@@ -6,7 +6,6 @@
 
 ## Load Packages
 library(tidyverse)
-library(curl)
 library(jsonlite)
 #library(soiltexture)
 #library(tictoc)
@@ -21,8 +20,8 @@ library(jsonlite)
 
 
 
-### 'get_data_soilgrids' function for download soilgriddata
-get_data_soilgrids <- function(soil_vars, lat, lon, depths){
+#### 'get_data_soilgrids' function for download soilgriddata
+#get_data_soilgrids_v1 <- function(soil_vars, lat, lon, depths){
     
     link <- paste0("https://rest.soilgrids.org/query?lon=", lon, "&lat=", lat, "&attributes=",
                    paste0(soil_vars, collapse = ","),"&depths=", paste0(depths, collapse = ","))
@@ -38,5 +37,115 @@ get_data_soilgrids <- function(soil_vars, lat, lon, depths){
     data %>% bind_rows(.id = "soil_vars") 
         
 }
+#
 
+#Default value : List [ "bdod", "cec", "cfvo", "clay", "nitrogen", "ocd", "ocs", "phh2o", "sand", "silt", "soc" ]
+soil_vars <- c("bdod", "cec", "cfvo", "clay", "nitrogen", "ocd", "ocs", "phh2o", "sand", "silt", "soc")
+#Default value : List [ "Q0.05", "Q0.5", "Q0.95", "mean", "uncertainty" ]
+value = c("Q0.5", "mean") 
+#Default value : List [ "0-5cm", "0-30cm", "5-15cm", "15-30cm", "30-60cm", "60-100cm", "100-200cm" ]
+depths = c("0-5cm", "5-15cm", "15-30cm", "30-60cm", "60-100cm", "100-200cm") 
 
+### 'get_data_soilgrids' function for download soilgriddata V2 https://www.isric.org/explore/soilgrids/faq-soilgrids 
+get_data_soilgrids <- function(lat, lon, soil_vars = c("bdod", "cfvo", "clay", "nitrogen", "sand", "silt", "soc"), value = c("mean"), depths = c("0-5cm", "5-15cm", "15-30cm", "30-60cm", "60-100cm", "100-200cm")){
+    
+    var_to_dl <- paste0("&property=", soil_vars, collapse = "")
+    depts_to_dl <- paste0("&depth=", depths, collapse = "")
+    values_to_ext <- paste0("&value=", value, collapse = "")
+    
+    
+    link <- paste0("https://rest.soilgrids.org/soilgrids/v2.0/properties/query?lon=", 
+                   lon, "&lat=", lat, 
+                   var_to_dl, 
+                   depts_to_dl, 
+                   values_to_ext)
+    
+    json_data <- fromJSON(link)
+    
+    
+ vars <- json_data$properties$layers$name
+ 
+ units <- json_data$properties$layers$unit_measure
+ 
+ mdata <- enframe(vars, value = "var", name = NULL) %>% 
+     bind_cols(units) %>% select(-uncertainty_unit)
+ 
+ data <- json_data$properties$layers$depths %>% 
+     set_names(vars) %>% 
+     bind_rows(.id = "var") %>% nest(data = c(range, label, values)) %>% 
+     right_join(mdata, by = "var")
+ 
+ 
+ return(data)
+}
+
+ 
+#soilgrids_data <- get_data_soilgrids(lat, lon, depths = '0-5cm')
+
+soilgrids_data %>% unnest(data) %>% 
+    select(var, range, label, values) %>% flatten() %>%
+    set_names(c("var", "tdepth","bdepth", "unit", "label", "value"))
+
+from_soilgrids_to_aquacrop <- function(id_name, soilgrids_data, Penetrability = 100) {
+    
+    # Depths of sl in soilgrids    
+    dept_value <- tibble(sd1 = -0.025, 
+                         sd2 = -0.1, 
+                         sd3 = -0.225, 
+                         sd4 = -0.45, 
+                         sd5 = -0.8, 
+                         sd6 = -1.5, 
+                         sl1 = 0.0, 
+                         sl2 = -0.05, 
+                         sl3 = -0.15, 
+                         sl4 = -0.3, 
+                         sl5 = -0.6, 
+                         sl6 = -1.0, 
+                         sl7 = -2.0, 
+                         xd1 = -0.2, 
+                         xd2 = -0.5) %>%
+        dplyr::select(contains("sl")) %>% 
+        gather(sl, depth)
+    
+    
+    ## transform data to aquacrop format
+    data_inp <- soilgrids_data %>% 
+        gather(sl, value, -soil_vars) %>% 
+        spread(soil_vars, value) %>%
+        left_join(dept_value, by="sl") %>%# rename(WCWP = WWP, WCST = AWCtS) %>%
+        mutate(Penetrability = Penetrability,
+               TKL = c(0, diff(abs(depth))),
+               WCFC = WWP + AWCh1,
+               SSKS = 75*24*10*(((((1-(BLDFIE/2650))*100)-WCFC))^2/(WCFC)^2),   #Method developed by Suleiman and Ritchie (2001)
+               STC = get_STC(SNDPPT, CLYPPT),
+               CRa = case_when(str_detect(STC, "Sa|LoSa|SaLo") ~ (-0.3112 - SSKS*10^(-5)),
+                               str_detect(STC, "Lo|SiLo|Si") ~ (-0.4986 + SSKS*9*10^(-5)),
+                               str_detect(STC, "SaCl|SaClLo|ClLo") ~ (-0.5677 - SSKS*4*10^(-5)),
+                               str_detect(STC, "SiClLo|SiCl|Cl") ~ (-0.6366 + SSKS*8*10^(-4))),
+               CRb = case_when(str_detect(STC, "Sa|LoSa|SaLo") ~ (-1.4936 + 0.2416*log(SSKS)),
+                               str_detect(STC, "Lo|SiLo|Si") ~ (-2.1320 + 0.4778*log(SSKS)),
+                               str_detect(STC, "SaCl|SaClLo|ClLo") ~ (-3.7189 + 0.5922*log(SSKS)),
+                               str_detect(STC, "SiClLo|SiCl|Cl") ~ (-1.9165 + 0.7063*log(SSKS)))) %>%
+        rename(WCWP = WWP, WCST = AWCtS, Gravel = CRFVOL) %>% 
+        dplyr::select(TKL, WCST, WCFC, WCWP, SSKS, Penetrability, Gravel, CRa, CRb, STC) %>%
+        setNames(c("Thickness", "Sat", "FC", "WP", "Ksat", "Penetrability", "Gravel", "CRa", "CRb", "description"))
+    
+    #CN: Curve number (dimensionless)
+    CN <- data_inp[1,] %>% 
+        mutate(CN = case_when(Ksat <= 10 ~ 85,
+                              Ksat > 10 & Ksat <=50 ~ 80,
+                              Ksat > 50 & Ksat <=250 ~ 75,
+                              Ksat > 250 ~ 65)) %>% pull(CN)
+    
+    
+    # REW: Readily Evaporable Water (mm)
+    REW <- data_inp[1,] %>%
+        mutate(REW_cal = (10*(FC - WP/2)*0.04),
+               REW = case_when(REW_cal >=15 ~ 15, 
+                               REW_cal < 0 ~ 0,
+                               TRUE ~ REW_cal)) %>% pull(REW) %>% sprintf("%1.f", .)
+    
+    
+    return(list(id_name = id_name, data = data_inp[-1,], CN = CN, REW = REW))
+    
+}
